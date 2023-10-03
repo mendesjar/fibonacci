53e4dac6415a80f6ec23d4aba968f9578e880e29
Gabriel Mendes <84147242+mendesjar@users.noreply.github.com>
2023-10-03 13:34:29 -0300

    Add files via upload
    build: add fibonacci calculation

---
+++ fibonacci.js
@@ -0,0 +1,20 @@
+console.time("Terminou");
+
+const calculoFibonacci = (numero) => {
+  let a = BigInt(0),
+    b = BigInt(1);
+
+  if (Number.isFinite(numero)) {
+    numero = 0;
+  }
+
+  for (let i = BigInt(0); i < numero; i++) {
+    [a, b] = [b, a + b];
+  }
+  return a.toString();
+};
+
+const calculo = calculoFibonacci(1);
+console.log(calculo);
+
+console.timeEnd("Terminou");
